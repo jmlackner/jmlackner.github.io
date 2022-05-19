2a861b7ce55f0b476b3678c3f12bf58f51b75fd9
jmlackner <lackner2@gmail.com>
2022-05-19 16:16:46 -0500

    logo update

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var currentDate = new Date();
 var weddingDate = new Date('12/10/2022');
-var daysToGo = (weddingDate.getTime() - currentDate.getTime()) / 24 / 60 / 60 / 1000;
+var daysToGo = Math.floor((weddingDate.getTime() - currentDate.getTime()) / 24 / 60 / 60 / 1000);
 
 document.getElementById('countdown').textContent = daysToGo + ' days to go!';
