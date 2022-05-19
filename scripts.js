7d1165984e57228cb2f756477b2df7a77ad55efd
jmlackner <lackner2@gmail.com>
2022-05-19 15:20:11 -0500

    CSS fix

--- scripts.js
+++ scripts.js
@@ -3,6 +3,4 @@ var weddingDate = new Date('12/10/2022');
 
 var daysToGo = weddingDate.getTime() - currentDate.getTime();
 
-const countdown = document.getElementById('countdown');
-
-countdown.textContent = daysToGo + ' days to go!';
+document.getElementById('countdown').textContent = daysToGo + ' days to go!';
